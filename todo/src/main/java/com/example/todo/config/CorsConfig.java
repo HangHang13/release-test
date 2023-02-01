5296610759ac9d3af00dc98e1f10bc13fcd989f7
HangHang13 <dhkdwlsgod@naver.com>
2023-02-01 15:25:56 +0900

    start

--- todo/src/main/java/com/example/todo/config/CorsConfig.java
+++ todo/src/main/java/com/example/todo/config/CorsConfig.java
@@ -12,9 +12,11 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         //api cors 정책 설정
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000") //api 요청 허용 URL
+                .allowedOrigins("http://localhost:3000","http://practice-hang-bucket.s3-website.ap-northeast-2.amazonaws.com") //api 요청 허용 URL
                 .allowedMethods("GET","POST","DELETE","PUT")
                 .allowedHeaders("*")
+
+
                 .allowCredentials(true) //자격증명하고와라
                 .maxAge(3600); //1시간
     }
